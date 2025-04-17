1311af87cefc44dd429c9b9e50a6601d511748a2
Naman Makkar <namanmakkar6@gmail.com>
2025-04-17 19:54:52 +0530

    env prisma setup

--- week13/medium/backend/src/index.ts
+++ week13/medium/backend/src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
 })
 
 export default app
